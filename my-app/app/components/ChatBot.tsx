2181e0f7c5c9e80aa54f74acef3de08dc5845dae
QuickReachAI <quickreach.ai@gmail.com>
2025-02-22 22:06:20 +0530

    Update ChatBot.tsx

--- my-app/app/components/ChatBot.tsx
+++ my-app/app/components/ChatBot.tsx
@@ -42,7 +42,7 @@ const ChatBot = () => {
 
     // If no predefined response, send the message to the backend API
     try {
-      const response = await fetch("http://127.0.0.1:4000/chatbot", {
+      const response = await fetch("https://api-zezh.onrender.com/chatbot", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ question: input }),
