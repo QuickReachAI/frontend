ebc7f6458dae99068b732998cd441375e3633eb8
QuickReachAI <quickreach.ai@gmail.com>
2025-06-17 23:47:39 +0530

    Update page.tsx

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -3,7 +3,7 @@ import Services from "./components/Services"
 import AboutUs from "./components/AboutUs"
 import WhyUs from "./components/WhyUs"
 import Portfolio from "./components/Portfolio"
-import Team from "./components/Team"
+// import Team from "./components/Team"
 import Contact from "./components/Contact"
 import FAQ from "./components/FAQ"
 export default function Home() {
@@ -14,7 +14,7 @@ export default function Home() {
       <AboutUs />
       <WhyUs />
       <Portfolio />
-      <Team />
+{/*       <Team /> */}
       <Contact />
       <FAQ/>
     </div>
